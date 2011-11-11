19b69becffb339dcad034a151d1de82a8e001926
aignospam <aignospam@gmail.com>
2011-11-11 10:33:02 +0400

    initial commit for rsb.ru

---
+++ src/htdocs/plugins/class.ib.rsb.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright (c) AIG
+ * aignospam at gmail.com
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+class IB_RSB extends IB {
+    private $_account_list;
+
+    public function __construct() {
+        parent::__construct("rsb");
+        $this -> _account_list = null;
+    }
+
+    public function login($user, $fish) {
+
+    }
+
+    public function getAccountList() {
+        return $this -> _account_list;
+    }
+
+    public function getAccountBalance($account_id, $inctran = false) {
+        $info = array();
+        $info['balance'] = 0;
+
+        return $info;
+    }
+
+}
+?>
