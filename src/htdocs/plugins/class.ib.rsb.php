510aa70651143bf9d15a5aee21e275320edde8f0
aig <aignospam@gmail.com>
2011-11-23 15:20:16 +0400

    add getAccountBalance support

--- src/htdocs/plugins/class.ib.rsb.php
+++ src/htdocs/plugins/class.ib.rsb.php
@@ -25,21 +25,29 @@
  * SUCH DAMAGE.
  */
 
-class IB_RSB extends IB {
+class IB_RSB extends IB
+{
     private $_account_list;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct("rsb");
+
         $this->_account_list = null;
+        $this->_account_name_list = null;
     }
 
-    public function login($user, $fish) {
+    public function login($user, $fish)
+    {
         // try to log in.
         $result = $this->curlLogin($user, $fish);
 
         if (!$result) return false;
 
-        if (!preg_match('/submitForm\(\'mainform\'\,1\,\{source:\'([^\']+)\'\}\)[^>]+>' . mb_convert_encoding('реквизиты счетов', 'HTML-ENTITIES', 'UTF-8') . '/', $result, $match)) {
+        if (!preg_match('/submitForm\(\'mainform\'\,1\,\{source:\'([^\']+)\'\}\)[^>]+>'
+                        . mb_convert_encoding('реквизиты счетов', 'HTML-ENTITIES', 'UTF-8')
+                        . '/', $result, $match)
+        ) {
             return false;
         }
 
@@ -47,14 +55,23 @@ class IB_RSB extends IB {
 
         $state_token = $this->getStateToken($result);
 
+        $post_fields = array();
+        $post_fields[] = 'oracle.adf.faces.FORM=mainform';
+        $post_fields[] = 'oracle.adf.faces.STATE_TOKEN=' . $state_token;
+        $post_fields[] = 'source=' . urlencode($source);
+
         $ch = curl_init();
+
         curl_setopt($ch, CURLOPT_URL, "https://online.rsb.ru/hb/faces/rs/RSIndex.jspx");
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "oracle.adf.faces.FORM=mainform&oracle.adf.faces.STATE_TOKEN={$state_token}&source=" . urlencode($source));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, implode('&', $post_fields));
 
         $result = $this->curlExec($ch);
 
-        if (!preg_match('/submitForm\(\'mainform\'\,1\,\{source:\'([^\']+)\'\}\)[^>]+>' . mb_convert_encoding('Пополняемые счета', 'HTML-ENTITIES', 'UTF-8') . '/', $result, $match)) {
+        if (!preg_match('/submitForm\(\'mainform\'\,1\,\{source:\'([^\']+)\'\}\)[^>]+>'
+                        . mb_convert_encoding('Пополняемые счета', 'HTML-ENTITIES', 'UTF-8')
+                        . '/', $result, $match)
+        ) {
             return false;
         }
 
@@ -62,10 +79,16 @@ class IB_RSB extends IB {
 
         $state_token = $this->getStateToken($result);
 
+        $post_fields = array();
+        $post_fields[] = 'oracle.adf.faces.FORM=mainform';
+        $post_fields[] = 'oracle.adf.faces.STATE_TOKEN=' . $state_token;
+        $post_fields[] = 'source=' . urlencode($source);
+
         $ch = curl_init();
+
         curl_setopt($ch, CURLOPT_URL, "https://online.rsb.ru/hb/faces/rs/RSIndex.jspx");
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "oracle.adf.faces.FORM=mainform&oracle.adf.faces.STATE_TOKEN={$state_token}&source=" . urlencode($source));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, implode('&', $post_fields));
 
         $result = $this->curlExec($ch);
 
@@ -76,16 +99,24 @@ class IB_RSB extends IB {
 
         $this->_account_list = array();
 
-        if (preg_match_all('/>((?:40817|42307|42306)\d{3}\d{12})/', $result, $matches)) {
-            foreach ($matches[1] as $account_id) {
-            $this->_account_list[] = new BankAccount($account_id);
-          }
+        $account_regexp = '/<span class="c3"><b>([^<]+)<\/b>.*?<span class="c3">((?:40817|42307|42306)\d{3}\d{12})/s';
+
+        if (preg_match_all($account_regexp, $result, $matches)) {
+            for ($i = 0; $i < count($matches[0]); $i++) {
+                $account_name = $matches[1][$i];
+                $account_id = $matches[2][$i];
+
+                $this->_account_name_list[$account_id] = $account_name;
+
+                $this->_account_list[] = new BankAccount($account_id);
+            }
         }
 
         return true;
     }
 
-    private function getStateToken($page) {
+    private function getStateToken($page)
+    {
         if (preg_match('/value="(\d+)"\s+name="oracle\.adf\.faces\.STATE_TOKEN"/', $page, $match)) {
             return $match[1];
         }
@@ -97,18 +128,41 @@ class IB_RSB extends IB {
         return false;
     }
 
-    public function getAccountList() {
+    public function getAccountList()
+    {
         return $this->_account_list;
     }
 
-    public function getAccountBalance($account_id, $inctran = false) {
+    public function getAccountBalance($account_id, $inctran = false)
+    {
+        if (!isset($this->_account_name_list[$account_id])) {
+            throw new Exception("Unknown bank account: {$account_id}");
+        }
+
+        $account_name = $this->_account_name_list[$account_id];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://online.rsb.ru/hb/faces/rs/RSIndex.jspx');
+
+        $result = $this->curlExec($ch);
+
+        $balance = 0;
+
+        if (preg_match('/>'
+                       . mb_convert_encoding($account_name, 'HTML-ENTITIES', 'UTF-8')
+                       . '<.*?class="money">(\d+(?:[\,\.]\d+)?)/s', $result, $match)
+        ) {
+            $balance = str_replace(',', '.', $match[1]);
+        }
+
         $info = array();
-        $info['balance'] = 0;
+        $info['balance'] = $balance;
 
         return $info;
     }
 
-    private function curlLogin($user, $fish) {
+    private function curlLogin($user, $fish)
+    {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://online.rsb.ru/hb/faces/system/rslogin.jsp");
         $result = $this->curlExec($ch);
@@ -125,4 +179,5 @@ class IB_RSB extends IB {
         return $result;
     }
 }
+
 ?>
